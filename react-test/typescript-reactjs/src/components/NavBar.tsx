a0c4f100f801566d8d365721eea362cb22c84a9e
Ken <imken0@outlook.com>
2024-03-28 17:42:48 -0400

    dynamiclaly updating the app title

--- react-test/typescript-reactjs/src/components/NavBar.tsx
+++ react-test/typescript-reactjs/src/components/NavBar.tsx
@@ -1,8 +1,24 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation  } from "react-router-dom";
+import React, { useEffect } from 'react';
 
+const getWebsiteTitle = (pathname : string) => {
 
+  if (pathname === '/') {
+    return 'Home';
+  } else if (pathname === '/grades') {
+    return 'Grades';
+  } else {
+    return 'Not Found';
+  }
+};
 
 const NavBar = () => {
+ const location = useLocation();
+
+  useEffect(() => {
+    document.title = `My Website | ${getWebsiteTitle(location.pathname)}`;
+  }, [location]);
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -10,6 +26,12 @@ const NavBar = () => {
         <Link to="/">
           <picture>
             <img src="https://cdn.7tv.app/emote/61f223a81704494956111846/2x.webp" />
+          </picture>
+        </Link>
+
+        <Link to="/">
+          <picture>
+            <img src="https://cdn.betterttv.net/emote/64acd705c2a236af8d29581a/2x.webp" />
           </picture>
         </Link>
 
