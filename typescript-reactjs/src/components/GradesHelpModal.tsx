3ca24fb3d98e3ecbab45b4d3c595e52837bcc8c1
Ken <imken0@outlook.com>
2024-04-04 23:07:29 -0400

    modal front end work fixed, added table for all grades and additional grades for non GPA grades

--- typescript-reactjs/src/components/GradesHelpModal.tsx
+++ typescript-reactjs/src/components/GradesHelpModal.tsx
@@ -5,6 +5,8 @@ import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { useSpring, animated } from '@react-spring/web';
+import { Table, TableHead, TableBody, TableRow, TableCell, Paper } from '@mui/material';
+
 
 interface FadeProps {
     children: React.ReactElement;
@@ -52,7 +54,7 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 700,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
@@ -83,12 +85,116 @@ const GradesHelpModal = ({ open, handleClose }: GradesHelpModal) => {
             >
                 <Fade in={open}>
                     <Box sx={style}>
-                        <Typography id="spring-modal-title" variant="h6" component="h2">
-                            Text in a modal
+                        <Typography id="spring-modal-title" variant="h5" component="h2" textAlign="center">
+                            Grading Scheme: Grade Point Values
                         </Typography>
-                        <Typography id="spring-modal-description" sx={{ mt: 2 }}>
-                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                        </Typography>
+
+                        {/* Empty space */}
+                        <div style={{ marginBottom: '20px' }} />
+
+                        <Paper>
+                            <Table size={"small"}>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Grade</TableCell>
+                                        <TableCell>Points</TableCell>
+                                        <TableCell>Equivalent Percentage</TableCell>
+                                        <TableCell>4.0 Scale</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell>A+</TableCell>
+                                        <TableCell>12</TableCell>
+                                        <TableCell>90-100</TableCell>
+                                        <TableCell>4</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>A</TableCell>
+                                        <TableCell>11</TableCell>
+                                        <TableCell>3.9</TableCell>
+                                        <TableCell>4</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>A-</TableCell>
+                                        <TableCell>10</TableCell>
+                                        <TableCell>80-84</TableCell>
+                                        <TableCell>3.7</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>B+</TableCell>
+                                        <TableCell>9</TableCell>
+                                        <TableCell>77-79</TableCell>
+                                        <TableCell>3.3</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>B</TableCell>
+                                        <TableCell>8</TableCell>
+                                        <TableCell>73-76</TableCell>
+                                        <TableCell>3</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>B-</TableCell>
+                                        <TableCell>7</TableCell>
+                                        <TableCell>70-72</TableCell>
+                                        <TableCell>2.7</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>C+</TableCell>
+                                        <TableCell>6</TableCell>
+                                        <TableCell>67-69</TableCell>
+                                        <TableCell>2.3</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>C</TableCell>
+                                        <TableCell>5</TableCell>
+                                        <TableCell>63-66</TableCell>
+                                        <TableCell>2</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>C-</TableCell>
+                                        <TableCell>4</TableCell>
+                                        <TableCell>60-62</TableCell>
+                                        <TableCell>1.7</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>D+</TableCell>
+                                        <TableCell>3</TableCell>
+                                        <TableCell>57-59</TableCell>
+                                        <TableCell>1.3</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>D</TableCell>
+                                        <TableCell>2</TableCell>
+                                        <TableCell>53-56</TableCell>
+                                        <TableCell>1</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>D-</TableCell>
+                                        <TableCell>1</TableCell>
+                                        <TableCell>50-52</TableCell>
+                                        <TableCell>0.7</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>F</TableCell>
+                                        <TableCell>0</TableCell>
+                                        <TableCell>0-49</TableCell>
+                                        <TableCell>0</TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+
+                            {/* Empty space */}
+                            <div style={{ marginBottom: '20px' }} />
+
+                            <Typography variant="body1" component="h2" >
+                                Additional Types of Grades:
+                                <ul>
+                                    <li>COM = Complete</li>
+                                    <li>CR = Credit Earned</li>
+                                </ul>
+                            </Typography>
+                        </Paper>
                     </Box>
                 </Fade>
             </Modal>
